cdfe33faa08a077d55cfbeed7b5a163a49409138
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2013-04-10 17:00:07 +0100

    Create a BatchAPI Class

--- class.libFacebookBatchAPI.php
+++ class.libFacebookBatchAPI.php
@@ -22,13 +22,13 @@ class FacebookBatchAPI {
         $params = array ( 'batch' => json_encode ( $this->calls ) );
         $params['access_token'] = $this->token;
         $url = 'https://graph.facebook.com/';
-        $ch = curl_init ( $url ) || throw new Exception ( 'Cannot initialise curl' );
-        curl_setopt ( $ch, CURLOPT_POST, 1 ) || throw new Exception ( 'Cannot set curl option CURLOPT_POST' );
-        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $params ) || throw new Exception ( 'Cannot set curl option CURLOPT_POSTFIELDS' );
-        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 ) || throw new Exception ( 'Cannot set curl option CURLOPT_RETURNTRANSFER' );
-        $this->rawdata = curl_exec ( $ch ) || throw new Exception ( 'Cannot execute curl connection' );
+        if ( empty ( $ch = curl_init ( $url ) ) ) { throw new Exception ( 'Cannot initialise curl' ); }
+        if ( empty ( curl_setopt ( $ch, CURLOPT_POST, 1 ) ) { throw new Exception ( 'Cannot set curl option CURLOPT_POST' ); }
+        if ( empty ( curl_setopt ( $ch, CURLOPT_POSTFIELDS, $params ) ) { throw new Exception ( 'Cannot set curl option CURLOPT_POSTFIELDS' ); }
+        if ( empty ( curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 ) ) { throw new Exception ( 'Cannot set curl option CURLOPT_RETURNTRANSFER' ); }
+        if ( empty ( $this->rawdata = curl_exec ( $ch ) ) { throw new Exception ( 'Cannot execute curl connection' ); }
         curl_close ( $ch );
-        $this->data = json_decode ( $this->data ) || throw new Exception ( 'Cannot parse output json' );
+        if ( empty ( $this->data = json_decode ( $this->data ) ) { throw new Exception ( 'Cannot parse output json' ); }
         $this->calls = array ();
         return $this->data;
     }
